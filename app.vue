ac3a24472fe9c7b716948d65cd24cc1e085866bf
Nick-tsoukas <nick.tsoukas101@gmail.com>
2024-11-28 07:59:59 -0800

    chore: setting up

---
+++ app.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="bg-white">
+    <header class="absolute inset-x-0 top-0 z-50">
+      <div class="mx-auto max-w-7xl">
+        <div class="px-6 pt-6 lg:max-w-2xl lg:pl-8 lg:pr-0">
+          <nav class="flex items-center justify-between lg:justify-start" aria-label="Global">
+            <a href="#" class="-m-1.5 p-1.5">
+              <span class="sr-only">Your Company</span>
+              <img alt="Your Company" class="h-8 w-auto" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600" />
+            </a>
+            <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700 lg:hidden" @click="mobileMenuOpen = true">
+              <span class="sr-only">Open main menu</span>
+              <Bars3Icon class="size-6" aria-hidden="true" />
+            </button>
+            <div class="hidden lg:ml-12 lg:flex lg:gap-x-14">
+              <a v-for="item in navigation" :key="item.name" :href="item.href" class="text-sm/6 font-semibold text-gray-900">{{ item.name }}</a>
+            </div>
+          </nav>
+        </div>
+      </div>
+      <Dialog class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
+        <div class="fixed inset-0 z-50" />
+        <DialogPanel class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div class="flex items-center justify-between">
+            <a href="#" class="-m-1.5 p-1.5">
+              <span class="sr-only">Your Company</span>
+              <img class="h-8 w-auto" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+            </a>
+            <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
+              <span class="sr-only">Close menu</span>
+              <XMarkIcon class="size-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div class="mt-6 flow-root">
+            <div class="-my-6 divide-y divide-gray-500/10">
+              <div class="space-y-2 py-6">
+                <a v-for="item in navigation" :key="item.name" :href="item.href" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">{{ item.name }}</a>
+              </div>
+              <div class="py-6">
+                <a href="#" class="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Log in</a>
+              </div>
+            </div>
+          </div>
+        </DialogPanel>
+      </Dialog>
+    </header>
+
+    <div class="relative">
+      <div class="mx-auto max-w-7xl">
+        <div class="relative z-10 pt-14 lg:w-full lg:max-w-2xl">
+          <svg class="absolute inset-y-0 right-8 hidden h-full w-80 translate-x-1/2 transform fill-white lg:block" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
+            <polygon points="0,0 90,0 50,100 0,100" />
+          </svg>
+
+          <div class="relative px-6 py-32 sm:py-40 lg:px-8 lg:py-56 lg:pr-0">
+            <div class="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl">
+             
+              <h1 class="text-pretty text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">Learn by Doing</h1>
+              <p class="mt-8 text-pretty text-lg font-medium text-gray-950 sm:text-xl/8">At Creativity Community School, we inspire students through hands-on projects and real-world challenges, fostering creativity, collaboration, and leadership for a brighter future.</p>
+              <div class="mt-10 flex items-center gap-x-6">
+                <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
+                <a href="#" class="text-sm/6 font-semibold text-gray-900">Learn more <span aria-hidden="true">→</span></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="bg-gray-50 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+        <img class="aspect-[3/2] object-cover lg:aspect-auto lg:size-full" src="@/assets/millwaukee.png" alt="" />
+      </div>
+    </div>
+    <!-- feature secont  -->
+    <div class=" py-24 sm:py-32">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+      <div class="mx-auto max-w-2xl lg:text-center">
+        <h2 class="mt-2 text-pretty text-5xl font-semibold tracking-tight text-black sm:text-5xl lg:text-balance">Lead by Innovating</h2>
+        <p class="mt-6 text-lg/8 text-black">At Creativity Community School, our mission is to empower students to explore their passions, think critically, and innovate boldly. Through experiential learning and a supportive community, we nurture the leaders and problem-solvers of tomorrow.</p>
+      </div>
+      <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
+        <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
+          <div v-for="feature in features" :key="feature.name" class="flex flex-col">
+            <dt class="flex items-center gap-x-3 text-base/7 font-semibold text-white">
+              <component :is="feature.icon" class="size-5 flex-none text-indigo-400" aria-hidden="true" />
+              {{ feature.name }}
+            </dt>
+            <dd class="mt-4 flex flex-auto flex-col text-base/7 text-black">
+              <p class="flex-auto">{{ feature.description }}</p>
+              <p class="mt-6">
+                <a :href="feature.href" class="text-sm/6 font-semibold text-indigo-400">Learn more <span aria-hidden="true">→</span></a>
+              </p>
+            </dd>
+          </div>
+        </dl>
+      </div>
+    </div>
+  </div>
+
+
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { Dialog, DialogPanel } from '@headlessui/vue'
+import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
+
+import { ArrowPathIcon, CloudArrowUpIcon, LockClosedIcon } from '@heroicons/vue/20/solid'
+
+const features = [
+  {
+    name: 'Push to deploy',
+    description:
+      'Commodo nec sagittis tortor mauris sed. Turpis tortor quis scelerisque diam id accumsan nullam tempus. Pulvinar etiam lacus volutpat eu.',
+    href: '#',
+    icon: CloudArrowUpIcon,
+  },
+  {
+    name: 'SSL certificates',
+    description:
+      'Pellentesque enim a commodo malesuada turpis eleifend risus. Facilisis donec placerat sapien consequat tempor fermentum nibh.',
+    href: '#',
+    icon: LockClosedIcon,
+  },
+  {
+    name: 'Simple queues',
+    description:
+      'Pellentesque sit elit congue ante nec amet. Dolor aenean curabitur viverra suspendisse iaculis eget. Nec mollis placerat ultricies euismod.',
+    href: '#',
+    icon: ArrowPathIcon,
+  },
+]
+
+const navigation = [
+  { name: 'Product', href: '#' },
+  { name: 'Features', href: '#' },
+  { name: 'Marketplace', href: '#' },
+  { name: 'Company', href: '#' },
+  { name: 'Log in', href: '#' },
+]
+
+const mobileMenuOpen = ref(false)
+</script>
